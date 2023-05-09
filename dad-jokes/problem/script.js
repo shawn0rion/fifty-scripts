ba1c26c0e7d5a7430340d07ddb82a3c73458ceb3
shawn0rion <shawnorion07@gmail.com>
2023-05-08 22:04:56 -0500

    get and display dad joke on btn click

---
+++ dad-jokes/problem/script.js
@@ -0,0 +1,23 @@
+const jokeButton = document.querySelector('.get-joke');
+const jokeDisplay = document.querySelector('.display');
+jokeButton.addEventListener('click', async () => {
+    try {
+        const joke = await fetchJoke();
+        jokeDisplay.textContent = joke;
+    } catch(error){
+        console.error(error);
+        jokeDisplay.textContent = 'What do you call an API error? Jason.'
+    }
+});
+
+async function fetchJoke(){
+    const url = 'https://icanhazdadjoke.com';
+    const response = await fetch(url, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const data = await response.json();
+    console.log(data);
+    return data.joke;
+}
